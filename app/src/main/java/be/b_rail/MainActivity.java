b96338137db1383b9b0ce6fe74d28fc077e675db
jwdapps <jwd.apps@gmail.com>
2016-06-04 15:54:58 +0200

    settings & mainactivity +++++

--- app/src/main/java/be/b_rail/MainActivity.java
+++ app/src/main/java/be/b_rail/MainActivity.java
@@ -1,5 +1,7 @@
 package be.b_rail;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -10,21 +12,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import be.b_rail.Utils.PrefsUtils;
 import be.b_rail.fragments.BaseFragment;
-import be.b_rail.fragments.HomeFragment;
+import be.b_rail.fragments.SncbFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout                drawer;
+    private TextView                    userName;
 
     private int 						mCurrentTitle;
     private int 						mSelectedFragment;
 
-    private static final int 			CASE_HOME 		  = 0;
+    private static final int 			CASE_SNCB 		  = 0;
     //Used in savedInstanceState
     private static String               BUNDLE_SELECTEDFRAGMENT 		= "BDL_SELFRG";
 
@@ -55,6 +59,8 @@ public class MainActivity extends AppCompatActivity
             NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
             navigationView.setNavigationItemSelectedListener(this);
 
+            userName	= (TextView) navigationView.getHeaderView(0).findViewById(R.id.textView_username);
+
             //-----------------------------------------------------------------
 
             if (savedInstanceState != null) {
@@ -64,13 +70,30 @@ public class MainActivity extends AppCompatActivity
                     selectFragment(mSelectedFragment);
 
             }else {
-                selectFragment(CASE_HOME);
+                selectFragment(CASE_SNCB);
+            }
+
+            AccountManager accountManager = AccountManager.get(this);
+            Account account = getAccount(accountManager);
+
+            if (account != null) {
+                //titleContact.setText(""+account.name);
+                userName.setText(""+account.name);
             }
 
       //  }
 
     }
-
+    private Account getAccount(AccountManager accountManager) {
+        Account[] accounts = accountManager.getAccountsByType("com.google");
+        Account account;
+        if (accounts.length > 0) {
+            account = accounts[0];
+        } else {
+            account = null;
+        }
+        return account;
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -109,23 +132,14 @@ public class MainActivity extends AppCompatActivity
         switch (id){
 
             //Replacing the main content with ContentFragment Which is our Inbox View;
-            case R.id.nav_home:
-                selectFragment(CASE_HOME);
-                return true;
-            case R.id.nav_camera:
-               // selectFragment(CASE_STATIONS);
-                return true;
-            case R.id.nav_slideshow:
-               // selectFragment();
-                return true;
-            case R.id.nav_manage:
-               // selectFragment();
+            case R.id.nav_sncb:
+                selectFragment(CASE_SNCB);
                 return true;
             case R.id.nav_share:
-               // startActivity( new Intent(MainActivity.this, SettingsActivity.class));
-                return true;
-            case R.id.nav_send:
                 //startActivity( new Intent(MainActivity.this, HelpActivity.class));
+                return true;
+            case R.id.nav_settings:
+                 startActivity( new Intent(MainActivity.this, SettingsActivity.class));
                 return true;
             default:
                 Toast.makeText(getApplicationContext(), "Somethings Wrong", Toast.LENGTH_SHORT).show();
@@ -136,7 +150,7 @@ public class MainActivity extends AppCompatActivity
 
         switch (position) {
 
-            case CASE_HOME:  openFragment(HomeFragment.newInstance("",""));  break;
+            case CASE_SNCB:  openFragment(SncbFragment.newInstance("",""));  break;
 
             default: break;
         }
